d9fd6358dcaf65be9928bfcac1f8a3037c6d205f
MajidALILOUCH <majidach2001@gmail.com>
2023-02-06 23:46:02 +0100

    adding Animation to our-skills section

--- main.js
+++ main.js
@@ -29,3 +29,16 @@ let counter = setInterval(() => {
     clearInterval(counter);
   }
 }, 1000);
+
+// Animate Width On Scrolling
+
+let section = document.querySelector(".our-skills");
+let spans = document.querySelectorAll(".the-progress span");
+
+window.onscroll = function () {
+  if (window.scrollY >= section.offsetTop - 300) {
+    spans.forEach((span) => {
+      span.style.width = span.dataset.width;
+    });
+  }
+};
