437ca3413e232569afbcf2cae5720b700a3a6dd4
MajidALILOUCH <majidach2001@gmail.com>
2023-02-08 17:23:51 +0100

    Increase Numbers On Scrolling

--- main.js
+++ main.js
@@ -42,3 +42,28 @@ window.onscroll = function () {
     });
   }
 };
+
+// Increase Numbers On Scrolling
+
+let nums = document.querySelectorAll(".box .number");
+let statSection = document.querySelector(".stats");
+let started = false; // Function Started ? No
+
+window.onscroll = function () {
+  if (window.scrollY >= statSection.offsetTop) {
+    if (!started) {
+      nums.forEach((num) => startCount(num));
+    }
+    started = true;
+  }
+};
+
+function startCount(el) {
+  let goal = el.dataset.goal;
+  let count = setInterval(() => {
+    el.textContent++;
+    if (el.textContent == goal) {
+      clearInterval(count);
+    }
+  }, 2000 / goal);
+}
